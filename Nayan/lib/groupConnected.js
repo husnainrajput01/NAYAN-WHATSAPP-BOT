634216ba14d4ccdae9031e12b52c5c9ce6997b05
Husnain Rajput <141842546+husnainrajput01@users.noreply.github.com>
2026-01-29 17:52:30 +0500

    Update groupConnected.js

--- Nayan/lib/groupConnected.js
+++ Nayan/lib/groupConnected.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   return `${global.config.botName} CONNECTED«
 
-Assalamualaykum☘️
+Assalamualaikum☘️
 <------------------------------>
 BOT CONNECTED SUCCESSFUL !!!
 
